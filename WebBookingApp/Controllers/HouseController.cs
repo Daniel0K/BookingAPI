3a65a0d2796dc033bd54bd8d52bb2aa5cf492b08
Daniel Kotov <sasukesharingan@mail.ru>
2022-02-10 14:55:59 +0700

    v1

---
+++ WebBookingApp/Controllers/HouseController.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Mvc;
+using WebBookingApp.models;
+
+namespace WebBookingApp.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HouseController : Controller
+{
+    private readonly ILogger<HouseController> _logger;
+
+    public HouseController(ILogger<HouseController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetHouses")]
+    public IEnumerable<House> Get()
+    {
+        var data = new[]
+        {
+            new House()
+            {
+                Id = 1,
+                X = 100,
+                Y = 100,
+                Bookings = new []
+                {
+                    new Booking() {StartDate = new DateTime(2022, 01, 02), EndDate = new DateTime(2022, 01, 02)},
+                },
+                IsActive = false,
+                IsNotConfirmed = false,
+                Name = "Россия",
+                Address = "Калининградская",
+                Desc = "Отличная улица"
+            }
+        };
+
+        return data;
+    }
+}
